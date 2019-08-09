4eb05aaef647d9e833abeea779fd63959333cd6d
sean <spcfox2@gmail.com>
2019-08-09 22:14:38 +1000

    initial fpga support

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+FPGA=0
 GPU=0
 CUDNN=0
 OPENCV=0
@@ -56,6 +57,12 @@ ifeq ($(CUDNN), 1)
 COMMON+= -DCUDNN 
 CFLAGS+= -DCUDNN
 LDFLAGS+= -lcudnn
+endif
+
+ifeq ($(FPGA), 1) 
+COMMON+= -DFPGA 
+CFLAGS+= -DFPGA
+#LDFLAGS+= -lcudnn
 endif
 
 OBJ=gemm.o utils.o cuda.o deconvolutional_layer.o convolutional_layer.o list.o image.o activations.o im2col.o col2im.o blas.o crop_layer.o dropout_layer.o maxpool_layer.o softmax_layer.o data.o matrix.o network.o connected_layer.o cost_layer.o parser.o option_list.o detection_layer.o route_layer.o upsample_layer.o box.o normalization_layer.o avgpool_layer.o layer.o local_layer.o shortcut_layer.o logistic_layer.o activation_layer.o rnn_layer.o gru_layer.o crnn_layer.o demo.o batchnorm_layer.o region_layer.o reorg_layer.o tree.o  lstm_layer.o l2norm_layer.o yolo_layer.o iseg_layer.o image_opencv.o
--- src/connected_layer.h
+++ src/connected_layer.h
@@ -6,11 +6,17 @@
 #include "network.h"
 
 layer make_connected_layer(int batch, int inputs, int outputs, ACTIVATION activation, int batch_normalize, int adam, int swa);
+void update_swa_connected_layer(layer l, update_args a);
 
+#ifdef FPGA
+void forward_connected_layer_fpga(layer l, network net);
+void backward_connected_layer_fpga(layer l, network net);
+void update_connected_layer_fpga(layer l, update_args a);
+#else
 void forward_connected_layer(layer l, network net);
 void backward_connected_layer(layer l, network net);
 void update_connected_layer(layer l, update_args a);
-void update_swa_connected_layer(layer l, update_args a);
+#endif
 
 #ifdef GPU
 void forward_connected_layer_gpu(layer l, network net);
@@ -19,6 +25,8 @@ void update_connected_layer_gpu(layer l, update_args a);
 void push_connected_layer(layer l);
 void pull_connected_layer(layer l);
 #endif
+
+
 
 #endif
 
